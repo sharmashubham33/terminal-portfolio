61fd36db942977c1b85c11654431c64455f9ac28
Shubham Sharma <shubham.sharma_adm@ellisdon.com>
2026-06-12 12:37:08 -0400

    fix(portfolio): support Vercel and GitHub Pages paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+declare const process: { env: { VERCEL?: string } };
+
+const base = process.env.VERCEL ? "/" : "/terminal-portfolio/";
+
 export default defineConfig({
-  base: "/terminal-portfolio/",
+  base,
   plugins: [react()],
 });
